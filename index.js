bedcac7a7c92f9608e3dc527772e7c1b7944c934
julio-qa <mail@julio.qa>
2025-02-06 12:12:47 +0000

    Limpeza e mudança da classe para Manager.

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 const dynamoose = require("dynamoose");
 
-class Model {
+class Manager {
     static #configured = false;
     static #models = {};
 
     static configure(config) {
-        if (Model.#configured) return;
+        if (Manager.#configured) return;
 
         if (!config || !config.region || !config.credentials?.accessKeyId || !config.credentials?.secretAccessKey) {
             throw new Error("Invalid AWS configuration.");
         }
 
         dynamoose.aws.ddb.set(new dynamoose.aws.ddb.DynamoDB(config));
-        Model.#configured = true;
+        Manager.#configured = true;
     }
 
     static define(modelName, schema, options = {}) {
-        if (!Model.#configured) {
+        if (!Manager.#configured) {
             throw new Error("Connection is not configured. Call Model.configure() first.");
         }
-        if (Model.#models[modelName]) {
-            return Model.#models[modelName];
+        if (Manager.#models[modelName]) {
+            return Manager.#models[modelName];
         }
         const schemaInstance = new dynamoose.Schema(schema, options);
         const model = dynamoose.model(modelName, schemaInstance);
         const modelAPI = {
             create: async (item) => {
-                await Model.#checkUniqueFields(model, schema, item);
+                await Manager.#checkUniqueFields(model, schema, item);
                 return model.create(item);
             },
             save: async (item) => {
-                await Model.#checkUniqueFields(model, schema, item, item.id);
+                await Manager.#checkUniqueFields(model, schema, item, item.id);
                 return model.update(item);
             },
             get: async (id) => {
@@ -42,7 +42,7 @@ class Model {
                 if (!existingItem) throw new Error("Item not found.");
 
                 const updatedItem = { ...existingItem.toJSON(), ...updateData };
-                await Model.#checkUniqueFields(model, schema, updatedItem, id);
+                await Manager.#checkUniqueFields(model, schema, updatedItem, id);
 
                 return model.update(updatedItem);
             },
@@ -51,7 +51,7 @@ class Model {
             }
         };
 
-        Model.#models[modelName] = modelAPI;
+        Manager.#models[modelName] = modelAPI;
         return modelAPI;
     }
 
@@ -71,87 +71,4 @@ class Model {
 
 }
 
-module.exports = { Model };
-
-// const dynamoose = require("dynamoose");
-//
-// class NonEasy {
-//     constructor(config) {
-//         if (!NonEasy.instance) {
-//             if (!config || !config.region || !config.credentials?.accessKeyId || !config.credentials?.secretAccessKey) {
-//                 throw new Error("Invalid AWS configuration.");
-//             }
-//
-//             // Configura DynamoDB apenas uma vez
-//             dynamoose.aws.ddb.set(new dynamoose.aws.ddb.DynamoDB(config));
-//
-//             this.models = {};
-//             NonEasy.instance = this;
-//         }
-//         return NonEasy.instance;
-//     }
-//
-//     getModel(name, schema, options = {}) {
-//         if (!this.models[name]) {
-//             this.models[name] = dynamoose.model(name, new dynamoose.Schema(schema, options));
-//         }
-//         return this.models[name];
-//     }
-// }
-//
-// let nonEasyInstance;
-// function initNonEasy(config) {
-//     if (!nonEasyInstance) {
-//         nonEasyInstance = new NonEasy(config);
-//     }
-//     return nonEasyInstance;
-// }
-//
-// module.exports = { initNonEasy };
-//
-
-
-
-// const dynamoose = require("dynamoose");
-//
-// class DB {
-//     constructor(model, schema, option, config = {}) {
-//         if (!config.region && !config.accessKeyId && !config.secretAccessKey) {
-//             throw new Error("Bad AWS config.");
-//         }
-//         dynamoose.aws.ddb.set(new dynamoose.aws.ddb.DynamoDB(config));
-//         this.schema = new dynamoose.Schema(schema, option);
-//         this.model = dynamoose.model(model, this.schema);
-//
-//         this.uniqueFields = Object.keys(schema).filter(
-//             key => schema[key].unique
-//         );
-//
-//     }
-//
-//     async create(item) {
-//         await this._checkUniqueFields(item);
-//         return this.model.create(item);
-//     }
-//
-//     async save(item) {
-//         await this._checkUniqueFields(item, item.id);
-//         return this.model.update(item);
-//     }
-//
-//     async _checkUniqueFields(item, excludeId = null) {
-//         for (const field of this.uniqueFields) {
-//             const value = item[field];
-//             if (value) {
-//                 const existingItem = await this.model.scan({ [field]: value }).exec();
-//                 if (existingItem.length > 0 && (!excludeId || existingItem[0].id !== excludeId)) {
-//                     throw new Error(`O valor '${value}' para o campo '${field}' já existe.`);
-//                 }
-//             }
-//         }
-//     }
-// }
-//
-// module.exports = {DB};
-
-
+module.exports = { Model: Manager };
